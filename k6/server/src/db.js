7463a89f1489854b1af55912e632b9df3d329955
Kalovelo <apostoloskalovelonis@gmail.com>
2026-05-18 15:10:46 +0300

    video improvements

--- k6/server/src/db.js
+++ k6/server/src/db.js
@@ -7,7 +7,7 @@ const pool = new Pool({
   user: process.env.DB_USER || "splitdumb",
   password: process.env.DB_PASSWORD || "splitdumb",
   max: 2,
-  connectionTimeoutMillis: 300,
+  connectionTimeoutMillis: 80,
 });
 
 async function initSchema() {
